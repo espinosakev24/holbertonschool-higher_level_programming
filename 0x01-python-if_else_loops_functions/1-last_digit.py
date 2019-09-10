ec26bddd5e72b3d0505ba4f6d86ade016ba72458
Kevin <916@holbertonschool.com>
2019-09-10 13:32:21 -0500

    function that checks positive and negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,13 @@ if number > 0:
 else:
     mod = number % -10
 if mod > 5:
-    print('Last digit of {:d} is {:d} and is greater than 5'.format(number, number % 10))
+    print('Last digit of {:d} is {:d} and\
+is greater than 5'.format(number, number % 10))
 elif mod == 0:
     print('Last digit of {:d} is {:d} and is 0'.format(number, number % 10))
 elif mod > 0 and mod < 6:
-    print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, number % 10))
+    print('Last digit of {:d} is {:d} and\
+is less than 6 and not 0'.format(number, number % 10))
 elif mod < 0:
-    print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, number % -10))
+    print('Last digit of {:d} is {:d} and\
+is less than 6 and not 0'.format(number, number % -10))
