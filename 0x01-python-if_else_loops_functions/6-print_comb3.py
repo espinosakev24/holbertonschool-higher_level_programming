b7f512b8e9b136f99336a2051ad9d7e7c576c56e
Kevin <916@holbertonschool.com>
2019-09-10 15:33:58 -0500

    program that prints all posible comb between two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for a in range(100):
     if a / 10 != a % 10 and a / 10 < a % 10 and a < 89:
         print('{:02d}, '.format(a), end="")
     elif a / 10 != a % 10 and a / 10 < a % 10 and a == 89:
-        print('{:02d}'.format(a))
+        print('{:02d} '.format(a))
