48546fe4f8336a34863a37cd636f001a744d013f
Kevin <916@holbertonschool.com>
2019-09-10 18:34:32 -0500

    Fizz Buzz function

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    x = 101
+    for a in range (1, x):
+        if a != x:
+            if a % 3 == 0 and a % 5 == 0:
+                print("FizzBuzz, ", end="")
+            elif a % 5 == 0:
+                if not(a == 100):
+                    print("Buzz, ", end="")
+                else:
+                    print("Buzz")  
+            elif a % 3 == 0:
+                print("Fizz, ", end="")
+            else:
+                print('{:d}, '.format(a), end="")
