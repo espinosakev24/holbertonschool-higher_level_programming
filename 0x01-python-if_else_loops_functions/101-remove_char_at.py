0f8c99d3d0dc8bb046dd8401b35f8c523a33649a
Kevin <916@holbertonschool.com>
2019-09-10 18:01:06 -0500

    function that removes a char in the index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    a = 0; new = ""
-    str_del = list(str)
-    del str_del[n]
-    for i in str_del:
-        new = new + str_del[a]
-        a += 1
-    return new
+    a = 0
+    new = ""
+    if n < 0:
+        return str
+    if not(n > len(str)):
+        str_del = list(str)
+        del str_del[n]
+        for i in str_del:
+            new = new + str_del[a]
+            a += 1
+        return new
+    else:
+        return str
