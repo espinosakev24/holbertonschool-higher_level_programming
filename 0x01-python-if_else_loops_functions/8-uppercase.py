5e861494981044fb388122222faca847a5147afe
Kevin <916@holbertonschool.com>
2019-09-10 23:34:11 -0500

    function that set string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    n = 0
-    for a in range(len(str)):
-        if ord(str[n]) >= 61 and ord(str[n]) <= 122:
-            print('{}'.format(chr(ord(str[n]) - 32)), end="")
-            n += 1
+    for a in str[:]:
+        if ord(a) >= 97 and ord(a) <= 122:
+            a = chr(ord(a) - 32)
+        print('{}'.format(a), end="")
     print()
