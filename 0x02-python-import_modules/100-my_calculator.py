52ae269a16963a666b3df5bf9d3eaa386b6b0339
Kevin <916@holbertonschool.com>
2019-09-12 18:28:36 -0500

    function that prints alphabet in uppercase

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,15 @@ if __name__ == "__main__":
     if len(argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] == '+':
-        print('{}'.format(add(int(argv[1]), int(argv[3]))))
-    if argv[2] == '-':
-        print('{}'.format(sub(int(argv[1]), int(argv[3]))))
-    if argv[2] == chr(42):
-        print('{}'.format(mul(int(argv[1]), int(argv[3]))))
-    if argv[2] == '/':
-        print('{}'.format(div(int(argv[1]), int(argv[3]))))
+    str = argv[2]
+    if str == '+' or str == '-' or str == '*' or str == '/':
+        if argv[2] == '+':
+            print('{}'.format(add(int(argv[1]), int(argv[3]))))
+        if argv[2] == '-':
+            print('{}'.format(sub(int(argv[1]), int(argv[3]))))
+        if argv[2] == chr(42):
+            print('{}'.format(mul(int(argv[1]), int(argv[3]))))
+        if argv[2] == '/':
+            print('{}'.format(div(int(argv[1]), int(argv[3]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
