80d3ca8c652476dfd484c4997d05c53902f1f914
Kevin <916@holbertonschool.com>
2019-09-12 12:39:37 -0500

    function that prints the amount of args and its name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print('{:d} arguments'.format(len(argv)))
-    for a in range(1, len(argv)):
-        print('{:d}: {:s}'.format(a, argv[a]))
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) > 1:
+        if len(argv) == 2:
+            print("{:d} argument.".format(len(argv) - 1))
+            print("{:d}: {:s}".format(len(argv) - 1, argv[1]))
+        else:
+            print('{} arguments:'.format(len(argv) - 1))
+            for a in range(1, len(argv)):
+                print('{:d}: {:s}'.format(a, argv[a]))
