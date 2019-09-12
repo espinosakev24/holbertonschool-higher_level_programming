0e1eb1b79cd84c420a5e481639273804a404ad78
Kevin <916@holbertonschool.com>
2019-09-12 18:42:11 -0500

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,9 +11,10 @@ if __name__ == "__main__":
             print('{}'.format(add(int(argv[1]), int(argv[3]))))
         if argv[2] == '-':
             print('{}'.format(sub(int(argv[1]), int(argv[3]))))
-        if argv[2] == chr(42):
+        if argv[2] == '*':
             print('{}'.format(mul(int(argv[1]), int(argv[3]))))
         if argv[2] == '/':
             print('{}'.format(div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
