1397addaaac13bf724dfc9c26754052e07c84214
Kevin <916@holbertonschool.com>
2019-09-11 20:54:05 -0500

    magic calc #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,7 @@
-#!/usr/bin/phyton3
 def magic_calculation(a, b, c):
-    return a * b * c
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
