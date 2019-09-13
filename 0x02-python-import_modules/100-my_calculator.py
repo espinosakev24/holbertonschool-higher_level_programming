200d02d94118aaa3ddddb672e642a50f1040c819
Kevin <916@holbertonschool.com>
2019-09-12 22:26:30 -0500

    Importing functions to build a calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,15 +5,18 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    s1 = argv[1]
+    s2 = argv[2]
+    s3 = argv[3]
     if argv[2] == '+' or argv[2] == '-' or argv[2] == '*' or argv[2] == '/':
         if argv[2] == '+':
-            print('{:d}'.format(add(int(argv[1]), int(argv[3]))))
+            print('{} {} {} = {:d}'.format(s1, s2, s3, add(int(s1), int(s3))))
         if argv[2] == '-':
-            print('{:d}'.format(sub(int(argv[1]), int(argv[3]))))
+            print('{} {} {} = {:d}'.format(s1, s2, s3, sub(int(s1), int(s3))))
         if argv[2] == '*':
-            print('{:d}'.format(mul(int(argv[1]), int(argv[3]))))
+            print('{} {} {} = {:d}'.format(s1, s2, s3, mul(int(s1), int(s3))))
         if argv[2] == '/':
-            print('{:d}'.format(div(int(argv[1]), int(argv[3]))))
+            print('{} {} {} = {:d}'.format(s1, s2, s3, div(int(s1), int(s3))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
