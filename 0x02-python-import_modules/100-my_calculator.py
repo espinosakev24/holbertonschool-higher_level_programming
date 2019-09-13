b3a2528a4f3792f77b6c475c61fb948dc2167267
Kevin <916@holbertonschool.com>
2019-09-12 22:12:51 -0500

    Importing functions to build a calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,16 +5,15 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    str = argv[2]
-    if str == '+' or str == '-' or str == '*' or str == '/':
+    if argv[2] == '+' or argv[2] == '-' or argv[2] == '*' or argv[2] == '/':
         if argv[2] == '+':
-            print('{}'.format(add(int(argv[1]), int(argv[3]))))
+            print('{:d}'.format(add(int(argv[1]), int(argv[3]))))
         if argv[2] == '-':
-            print('{}'.format(sub(int(argv[1]), int(argv[3]))))
+            print('{:d}'.format(sub(int(argv[1]), int(argv[3]))))
         if argv[2] == '*':
-            print('{}'.format(mul(int(argv[1]), int(argv[3]))))
+            print('{:d}'.format(mul(int(argv[1]), int(argv[3]))))
         if argv[2] == '/':
-            print('{}'.format(div(int(argv[1]), int(argv[3]))))
+            print('{:d}'.format(div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
