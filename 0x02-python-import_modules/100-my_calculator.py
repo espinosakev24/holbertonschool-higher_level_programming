bcb7dd3ee9ace5aa1823441c6e397db2a637768b
Kevin <916@holbertonschool.com>
2019-09-12 22:03:18 -0500

    Importing functions to build a calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    if len(argv) < 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     str = argv[2]
@@ -11,8 +11,7 @@ if __name__ == "__main__":
             print('{}'.format(add(int(argv[1]), int(argv[3]))))
         if argv[2] == '-':
             print('{}'.format(sub(int(argv[1]), int(argv[3]))))
-        if argv[2] == '\*':
-            
+        if argv[2] == '*': 
             print('{}'.format(mul(int(argv[1]), int(argv[3]))))
         if argv[2] == '/':
             print('{}'.format(div(int(argv[1]), int(argv[3]))))
