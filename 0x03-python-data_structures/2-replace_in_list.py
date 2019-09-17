b451efe92bb101d1459b3862a16b405966776caa
Kevin <916@holbertonschool.com>
2019-09-17 18:53:15 +0000

    uploading remplace element task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -9,11 +9,3 @@ def replace_in_list(my_list, idx, element):
 				my_list[a] = element
 				return my_list
 			a += 1
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
-
