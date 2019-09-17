9f7ba1dd670c3fe7715eff3f88d296263a2d3d8c
Kevin <916@holbertonschool.com>
2019-09-17 23:29:23 +0000

    function that deletes a element of a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not(idx < 0 or len(my_list) < idx):
-       del my_list[idx]
-       return my_list
-    else:
+    if not(idx < 0 or len(my_list) <= idx):
+        del my_list[idx]
         return my_list
+    return my_list
