b4083ccba768cdf347560c1fce5ceca255ae0caf
Kevin <916@holbertonschool.com>
2019-09-17 19:11:35 +0000

    remplacing a element in a list without modyfing the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     a = 0
     new_list = my_list[:]
-    if idx > len(my_list) or idx < 0:
+    if idx => len(my_list) or idx < 0:
         return new_list
     else:
         for n in new_list:
