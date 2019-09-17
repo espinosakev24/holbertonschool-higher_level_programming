0d87b28b6476fbd4ad992b1a4d521db840ae7c61
Kevin <916@holbertonschool.com>
2019-09-17 19:00:53 +0000

    uploading remplace element task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-	a = 0
-	if idx < 0 or idx > len(my_list):
-		return my_list
-	else:
-		for n in my_list:
-			if idx == a:
-				my_list[a] = element
-				return my_list
-			a += 1
+    a = 0
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+        else:
+                for n in my_list:
+                    if idx == a:
+                        my_list[a] = element
+                        return my_list
+                    a += 1
