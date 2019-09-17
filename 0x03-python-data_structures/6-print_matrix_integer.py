ee4979b602f98a879e388e496b11ad311cc1403a
Mari <916@holbertonschool.com>
2019-09-17 11:27:46 -0500

    uploading projects from 0 to 5 and adding task #6 --> need to delete end spaces between rows

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for a in matrix:
+        for b in a:
+            print('{:d} '.format(b), end="")
+        print ()
+matrix = [
+            [1, 2, 3],
+                [4, 5, 6],
+                    [7, 8, 9]
+                    ]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
