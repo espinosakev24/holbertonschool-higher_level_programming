a611ad221d9e71c8026f02a23ff62eab03f602da
Kevin <916@holbertonschool.com>
2019-09-19 12:59:09 -0500

    function that returns common elements between two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new = set_1 & set_2
+    return new
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
