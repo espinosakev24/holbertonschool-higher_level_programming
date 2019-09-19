310f8b45721f0b3fed689fb297b5feddbb9f0a20
Kevin <916@holbertonschool.com>
2019-09-19 12:05:16 -0500

    function that repaces occurrences in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    n = 0
+    cpy = my_list[:]
+    new = []
+    for a in cpy:
+        if a == search:
+            del cpy[n]
+            new.append(replace)
+        new.append(cpy[n])
+        n += 1
+    new.append(replace)
+    return new
