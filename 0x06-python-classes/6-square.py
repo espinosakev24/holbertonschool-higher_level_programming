271bc7bed8c8fc7fddfc74f55253e73b5ff1bab4
Kevin <916@holbertonschool.com>
2019-10-01 20:08:07 -0500

    Method class that prints a square in x and y position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(self.__position) != 2 or not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for d in range(self.__position[1]):
+            print()
+        for a in range(self.__size):
+            for c in range(self.__position[0]):
+                print(' ', end="")
+            for b in range(self.__size):
+                print('{}'.format('#'), end="")
+            print()
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
