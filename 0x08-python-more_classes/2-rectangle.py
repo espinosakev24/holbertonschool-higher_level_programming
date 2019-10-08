979372ec7f78027d30f5cc0c5374b5a611c17555
Kevin <916@holbertonschool.com>
2019-10-07 22:39:05 -0500

    fixing 2 rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,19 +10,20 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, value):
-        if not(isinstance(value, int)):
+    def width(self, width):
+        if not isinstance(width, int):
             raise TypeError('width must be an integer')
-        if value < 0:
+        if width < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
-        if not(isinstance(value, int)):
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
             raise TypeError('height must be >= 0')
