f146dac38db295762043d5193342da04e466580a
Kevin <916@holbertonschool.com>
2019-10-16 03:58:10 +0000

    uploading task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Module of"""
+
+
+class BaseGeometric:
+    """defining a empty class"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometric):
+    """Defining class Rectangle"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Defininf class square"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+s = Square(13)
+
+print(s)
+print(s.area())
