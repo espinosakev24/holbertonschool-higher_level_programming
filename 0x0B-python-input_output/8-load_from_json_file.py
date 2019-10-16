528cce57a7e2d83f64bd700852c6c1811b910f30
Kevin <916@holbertonschool.com>
2019-10-16 23:22:19 +0000

    load from json file function

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """load from json file function"""
     text = ""
     with open(filename, "r", encoding="utf-8") as fd:
         text = fd.read()
