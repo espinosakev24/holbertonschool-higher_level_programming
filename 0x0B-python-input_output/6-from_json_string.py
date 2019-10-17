fcff55d8eb4d9f33f516c5da794c0d485d050792
Kevin <916@holbertonschool.com>
2019-10-17 02:03:55 +0000

    fixing pep8 issue

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """module of from_json_string"""
-
-
 import json
+
+
 def from_json_string(my_str):
     """function to convert from js str to py obj"""
     return json.loads(my_str)
