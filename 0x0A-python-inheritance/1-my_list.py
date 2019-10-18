3b9c0f0dcd595b9237109a171e81a1328373b6a1
Kevin <916@holbertonschool.com>
2019-10-17 19:57:02 -0500

    function that prints a list sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Module of """
+"""Module of my list"""
 
 
 class MyList(list):
     """Function MyList"""
 
     def print_sorted(self):
+        """
+        Print sorted
+        """
         new = self.copy()
         new.sort()
         print(new)
