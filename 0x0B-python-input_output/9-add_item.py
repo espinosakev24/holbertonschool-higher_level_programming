d5c633ba9d4bc3d486ceedce8d7112f1bc8ef0d0
Kevin <916@holbertonschool.com>
2019-10-17 19:39:46 -0500

    uploading task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,10 +3,10 @@
 from sys import argv
 from os.path import isfile
 arch = "add_item.json"
-if not isfile(arch):
-    save_to_json_file([], arch)
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+if not isfile(arch):
+    save_to_json_file([], arch)
 new_list = load_from_json_file(arch)
 for a in range(1, len(argv)):
     new_list.append(argv[a])
