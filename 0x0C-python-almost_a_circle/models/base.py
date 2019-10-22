75002c53bfeca8712990cb1515a87a67ac60f4c4
Kevin <916@holbertonschool.com>
2019-10-22 18:23:08 -0500

    uploading saving and loading from csv file proj

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """Module of Base Class"""
-
-
 import json
 import os.path
 
 
 class Base:
+
     """Clase Base"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """constructor of the class
-        """
         self.id = id
         if self.id is not None:
             self.id = id
@@ -21,38 +18,38 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    """ Task --> 15 Dict to Json string """
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Task --> 15 Dict to Json string
-        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
 
+    """ Task --> 16 Json string to file """
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Task --> 16 Json string to file """
-        new = []
         arch = cls.__name__ + ".json"
-        if list_objs is not None:
-            for a in list_objs:
-                new.append(cls.to_dictionary(a))
-
         with open(arch, "w", encoding="utf-8") as fd:
-            fd.write(cls.to_json_string(new))
+            if list_objs == []:
+                fd.write('[]')
+            new = []
+            for a in list_objs:
+                new.append(a.to_dictionary())
+            content = cls.to_json_string(new)
+            fd.write(content)
 
+    """ Task --> 17 Json string to dict """
     @staticmethod
     def from_json_string(json_string):
-        """ Task --> 17 Json string to dict """
         if json_string is None or len(json_string) == 0:
             return []
         else:
             return json.loads(json_string)
 
+    """ Task --> 18 Dictionary to instance """
     @classmethod
     def create(cls, **dictionary):
-        """ Task --> 18 Dictionary to instance """
         if cls.__name__ == "Square":
             new = cls(1)
         if cls.__name__ == "Rectangle":
@@ -60,12 +57,43 @@ class Base:
         new.update(**dictionary)
         return new
 
+    """ Task --> 19 return list of instances """
     @classmethod
     def load_from_file(cls):
-        """ Task --> 19 return list of instances """
         new_list = []
         new_content = ""
         file_name = cls.__name__ + ".json"
+        if not os.path.exists(file_name):
+            return []
+        else:
+            with open(file_name, "r", encoding="utf-8") as fd:
+                content = fd.read()
+                new_content = cls.from_json_string(content)
+                for a in new_content:
+                    new_list.append(cls.create(**a))
+            return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ task --> advanced 1
+        """
+        arch = cls.__name__ + ".csv"
+        with open(arch, "w", encoding="utf-8") as fd:
+            if list_objs == []:
+                fd.write('[]')
+            new = []
+            for a in list_objs:
+                new.append(a.to_dictionary())
+            content = cls.to_json_string(new)
+            fd.write(content)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ task --> advanced 1
+        """
+        new_list = []
+        new_content = ""
+        file_name = cls.__name__ + ".csv"
         if not os.path.exists(file_name):
             return []
         else:
@@ -82,8 +110,8 @@ class Base:
         import turtle
 
         sq = turtle.Turtle()
-        sq.shape('turtle')
-        sq.speed(3)
+        sq.shape('square')
+        sq.speed(1)
         a = list_rectangles[0]
         sq.color('green')
         for a in list_rectangles:
