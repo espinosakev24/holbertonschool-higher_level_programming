8c4609883d99b7190053d1b5bc530f660acbfa9d
Kevin <916@holbertonschool.com>
2019-10-22 15:52:06 -0500

    fixing validations in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,10 +30,6 @@ class Square(Rectangle):
     def size(self, value):
         """ method size
         """
-        if type(value) is not int:
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise TypeError('width must be > 0')
         self.width = value
         self.height = value
 
