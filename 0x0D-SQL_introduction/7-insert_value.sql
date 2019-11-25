d958d862026f7ccfb1719fbe971c01121bddcf91
espinosakev24 <916@holbertonschool.com>
2019-11-25 15:01:10 -0500

    inserting atr and values to the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- inserting row to a table
 -- adding id and name to the table
-INSERT INTO first_table (id, name) VALUES (98, 'Holberton School');
+INSERT INTO first_table (id, name)
+VALUES (89, 'Holberton School');
