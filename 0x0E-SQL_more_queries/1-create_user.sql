b7491fc2459aabe50bf1de2546206fca3abefbd6
espinosakev24 <916@holbertonschool.com>
2019-11-26 13:01:02 -0500

    uploading task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Script that creates a database user
--- Created only for user - all permmisions
+-- Script that creates an user
+-- Created only for user_0d_1 - all permmisions
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1d_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
