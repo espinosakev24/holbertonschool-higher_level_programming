c19a202b9a5b2a24ad25999ad7dbe457ba68bc8e
espinosakev24 <916@holbertonschool.com>
2019-11-26 12:48:51 -0500

    uploading task from 0 to 2 about more queries ---> 3 tasks from creating to granting permessions to users

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates a database user
+-- Created only for user - all permmisions
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1d_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
