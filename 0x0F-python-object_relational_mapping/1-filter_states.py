6cf6ad818bd7b743b888f0447f5ae9e1ea818afc
espinosakev24 <916@holbertonschool.com>
2019-12-15 09:27:15 -0500

    Fixes -> missing closing the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     a1, a2, a3 = argv[1], argv[2], argv[3]
     db = sql.connect(host="localhost", db=a3, user=a1, passwd=a2, port=3306)
     query = "SELECT * FROM states WHERE name REGEXP '^[N]'" \
-            "ORDER BY states.id ASC"
+            "ORDER BY states.id"
     cur = db.cursor()
     cur.execute(query)
     for data in cur.fetchall():
         print(data)
 
+    cur.close()
     db.close()
