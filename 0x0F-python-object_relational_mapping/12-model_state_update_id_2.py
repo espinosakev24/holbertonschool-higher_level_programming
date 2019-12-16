5e3f7f5ed07a0b9dcb37505eacb2f42d6a9f0a31
espinosakev24 <916@holbertonschool.com>
2019-12-15 23:22:41 -0500

    script that updates a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    a1, a2, a3 = argv[1], argv[2], argv[3]
+    a = 0
+    engine = create_engine('mysql+mysqldb://{}:{}'
+                           '@localhost/{}'
+                           .format(a1, a2, a3, pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    instance = s.query(State).filter_by(id=2).first()
+    if instance.id:
+        instance.name = 'New Mexico'
+    s.commit()
+    s.close()
