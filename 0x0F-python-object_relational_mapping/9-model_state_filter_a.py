be20c2e0bbd5f2ad6377bcded119226671158ae0
espinosakev24 <916@holbertonschool.com>
2019-12-15 22:24:54 -0500

    script that prints all states containing an 'a' in their names

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    a1, a2, a3 = argv[1], argv[2], argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}'
+                           '@localhost/{}'
+                           .format(a1, a2, a3, pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    for instance in s.query(State).order_by(State.id):
+        if 'a' in instance.name:
+            print('{}: {}'.format(instance.id, instance.name))
