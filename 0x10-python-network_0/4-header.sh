54beaf406a6a3e7b90bc460385b3ea875a089305
espinosakev24 <916@holbertonschool.com>
2020-01-06 17:59:05 -0500

    sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a header variable
-curl -s -H "$1?X-HolbertonSchool-User-Id=98"
+# Script that sends a header variable.
+curl -s -H "X-HolbertonSchool-User-Id=98" "$1"
