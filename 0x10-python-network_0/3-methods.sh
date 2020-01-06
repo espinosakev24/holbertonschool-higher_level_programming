1e24a78b93eebdd4d95bd4c733484fa10dc07dfd
espinosakev24 <916@holbertonschool.com>
2020-01-06 17:25:27 -0500

    displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a OPTION request and display allow methods
-curl -is -X OPTIONS $1 | sed -n "/Allow:/p" | cut -d ":" -f 2
+curl -is -X OPTIONS $1 | sed -n "/Allow:/p" | cut -d ' ' -f2-
