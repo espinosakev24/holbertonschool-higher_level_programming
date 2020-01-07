fbf085f8b69aff8d7dfffae76816c8202ebc846d
espinosakev24 <916@holbertonschool.com>
2020-01-06 22:56:45 -0500

    script that performs a request POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a Post request to a URL
-curl -sH -X POST "email:hr@holbertonschool.com%subject:I will always be here for PLD" "$1"
+curl -sH -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
