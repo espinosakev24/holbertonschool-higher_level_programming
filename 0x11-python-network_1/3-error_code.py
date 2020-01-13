829de2609cf7f336f10f43bc683ca58ceaa00cb7
espinosakev24 <916@holbertonschool.com>
2020-01-13 16:39:26 -0500

    sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request
+to the URL and displays the body of the response
+"""
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+                print("Error code: {}".format(error.code))
