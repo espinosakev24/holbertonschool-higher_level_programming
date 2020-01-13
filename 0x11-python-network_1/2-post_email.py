3b42b41e2b8392c2f6baa45af16e78836a31737b
espinosakev24 <916@holbertonschool.com>
2020-01-13 15:56:47 -0500

    sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that sends a POST request to the
-passed URL with the email as a parameter,
+Script that sends a POST request to the-
+passed URL with the email as a parameter.
 """
 import urllib.parse
 import urllib.request
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         body = response.read()
