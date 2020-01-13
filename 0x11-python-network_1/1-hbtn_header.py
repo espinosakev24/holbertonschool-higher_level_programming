b7a173466d886a79f1ad562d0c0724b0589ddb97
espinosakev24 <916@holbertonschool.com>
2020-01-13 14:12:53 -0500

    Script that displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,5 +4,5 @@ import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.info()['X-Request-Id'])
+    print(response.info().get('X-Request-Id'))
     response.close()
