04a871a0d3ff6af8f471f2618f8496c8e68a304b
espinosakev24 <916@holbertonschool.com>
2020-01-13 23:29:18 -0500

    takes your Github credentials (username and password) and uses the Github API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     url = "https://api.github.com/users"
     username = argv[1]
     password = argv[2]
-    try:
-        req = requests.get(url, auth=(username, password))
-        print(req.json()[0].get('id'))
-    except Exception:
+    req = requests.get(url, auth=(username, password))
+    if "id" in req.json():
+        print(req.json().get('id'))
+    else:
         print(None)
