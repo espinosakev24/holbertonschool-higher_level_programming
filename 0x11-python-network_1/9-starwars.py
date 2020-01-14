31163856c43af55040ddf5ed0e68a97bdf63e458
espinosakev24 <916@holbertonschool.com>
2020-01-13 19:07:39 -0500

    takes in a string and sends a search request to the Star Wars API

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -6,8 +6,9 @@ a search request to the Star Wars API
 from sys import argv
 import requests
 
-url = "https://swapi.co/api/people/?search={}".format(argv[1])
-req = requests.get(url).json()
-print("Number of results: {}".format(req['count']))
-for data in req['results']:
-    print("{}".format(data['name']))
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/?search={}".format(argv[1])
+    req = requests.get(url).json()
+    print("Number of results: {}".format(req['count']))
+    for data in req['results']:
+        print("{}".format(data['name']))
