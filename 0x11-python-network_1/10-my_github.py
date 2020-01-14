e1cd167db16e7ae883e7a325f018625970c2aeff
espinosakev24 <916@holbertonschool.com>
2020-01-13 23:36:33 -0500

    takes your Github credentials (username and password) and uses the Github API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,17 +2,17 @@
 """
 script that takes your Github credentials
 (username and password) and uses the Github
-API to display your id
+API to display your id.
 """
 from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = "https://api.github.com/users"
+    url = "https://api.github.com/user"
     username = argv[1]
     password = argv[2]
-    req = requests.get(url, auth=(username, password))
-    if "id" in req.json():
-        print(req.json().get('id'))
-    else:
+    try:
+        req = requests.get(url, auth=(username, password))
+        print(req.json()[0].get('id'))
+    except Exception:
         print(None)
