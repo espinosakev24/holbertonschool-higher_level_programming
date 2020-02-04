9f1c8b0e41a1977bdb6d48741539509a3ea17604
espinosakev24 <916@holbertonschool.com>
2020-02-04 18:21:16 +0000

    changes loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-arr.forEach((value) => {
-  console.log(`${value}`);
-});
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let idx = 0; idx < arr.lenght; idx++) {
+  console.log(arr[idx]);
+}
