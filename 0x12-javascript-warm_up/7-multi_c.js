3ae34b0792e34c8e427d244befbf9e0f98c7bd9f
espinosakev24 <916@holbertonschool.com>
2020-02-04 18:39:04 +0000

    fixes styles

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,10 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x) === true) {
   console.log('Missing number of occurrences');
 } else {
-  while (idx !== x) {
-    idx = idx + 1;
-    console.log('C is fun');
+  if (x > 0) {
+    while (idx !== x) {
+      idx = idx + 1;
+      console.log('C is fun');
+    }
   }
 }
