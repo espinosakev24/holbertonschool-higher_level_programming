0f34ed6271618231bf618d52274c2676d4538828
espinosakev24 <916@holbertonschool.com>
2020-02-04 12:46:04 -0500

    fixes requ

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.length;
-if (args <= 2)
-    console.log("No argument");
-else
-    console.log(process.argv[2]);
+const args = process.argv.slice(2);
+if (args[0] == undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
