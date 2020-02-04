364b89bdf7bc560e0fa696b4c5565c3b7d142b46
espinosakev24 <916@holbertonschool.com>
2020-02-04 18:29:15 +0000

    fixes styles

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let string in arr) {
+for (const string in arr) {
   console.log(arr[string]);
 }
