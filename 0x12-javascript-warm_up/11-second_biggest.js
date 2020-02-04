9c18e564a1a7222cd1bf69fa7cc50572caa88d68
espinosakev24 <916@holbertonschool.com>
2020-02-04 23:40:30 +0000

    adds condition for arr without max value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,5 +8,9 @@ if (arr.length === 0 || arr.length === 1) {
   });
   const max = Math.max.apply(Math, arr);
   arr.splice(arr.indexOf(max));
-  console.log(Math.max.apply(Math, arr));
+  if (arr.length === 0) {
+    console.log(undefined);
+  } else {
+    console.log(Math.max.apply(Math, arr));
+  }
 }
