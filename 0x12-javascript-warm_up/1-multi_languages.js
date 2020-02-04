d65de72bac5f327aac797d3ca65835e36c90faca
espinosakev24 <916@holbertonschool.com>
2020-02-04 17:31:00 +0000

    fixes style requirements

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const C = "C is fun", P = "Python is cool", J = "Javascript is amazing";
+const C = 'C is fun';
+const P = 'Python is cool';
+const J = 'Javascript is amazing';
 console.log(C);
 console.log(P);
 console.log(J);
