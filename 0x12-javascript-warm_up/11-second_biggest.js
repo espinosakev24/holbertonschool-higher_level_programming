a07fe87801a6b736b98fc2637f15ee44b671f6c0
espinosakev24 <916@holbertonschool.com>
2020-02-05 00:32:21 +0000

    fixes task 8

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 let arr = process.argv.slice(2);
 if (arr.length === 0 || arr.length === 1) {
-  console.log(0);
+  console.log(String(0));
 } else {
   arr = arr.map(function (x) {
     return Number(x);
   });
   const max = Math.max.apply(Math, arr);
   arr.splice(arr.indexOf(max));
-  console.log(String(Math.max.apply(Math, arr)));
+  if (arr.length === 0) {
+    console.log(String(max));
+  } else {
+    console.log(String(Math.max.apply(Math, arr)));
+  }
 }
