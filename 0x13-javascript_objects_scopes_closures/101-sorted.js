e2ae382387514490e506f6f9e8bc66962fbd8dd7
espinosakev24 <kevinespinosazapata@gmail.com>
2021-03-24 11:58:14 -0500

    adds advance exercise

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,8 +2,11 @@
 const d = require('./101-data.js').dict;
 const dictKeys = {};
 
-Array.from(new Set(Object.values(d))).forEach(i => dictKeys[i] = []);
-for (n in d) {
+Array.from(new Set(Object.values(d))).forEach(i => {
+    dictKeys[i] = [];
+    return;
+});
+for (const n in d) {
     dictKeys[d[n]].push(n);
-};
+}
 console.log(dictKeys);
